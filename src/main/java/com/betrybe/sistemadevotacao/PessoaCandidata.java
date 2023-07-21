2ef936f94fb6c3764926428b9b1a3721d5eee324
Patricia Eliseu <patyeliseu4@gmail.com>
2023-07-21 19:30:27 -0300

    add requisito 01 classe abstrata Pessoa

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,38 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  public String nome;
+  public int numero;
+  public int votos;
 
+  @Override
+  public String getNome() {
+    return nome;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  int receberVoto() {
+    votos += 1;
+    return 0;
+  }
 }
