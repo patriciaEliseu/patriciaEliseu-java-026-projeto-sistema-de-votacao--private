6833c93ee3f8f456d5e37f167a5677cd347da65d
Patricia Eliseu <patyeliseu4@gmail.com>
2023-07-24 12:29:08 -0300

    add gerenciamentoVotaçao req.4

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,25 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+public abstract class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<String> pessoasCandidatas;
+  private ArrayList<String> cpfsComputados;
+  private ArrayList<String> pessoasEleitoras;
+
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  public void mostrarResultado() {
+
+  }
 }
