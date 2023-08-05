c8c46ce8592b9f68ba0e5246b2c59ce80226e543
Patricia Eliseu <patyeliseu4@gmail.com>
2023-08-04 22:00:43 -0300

    add req. 5 implementar os metodos cadastrarCandidatos e cadastrarEleitores

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -2,22 +2,48 @@ package com.betrybe.sistemadevotacao;
 
 import java.util.ArrayList;
 
-public abstract class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
-  private ArrayList<String> pessoasCandidatas;
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
   private ArrayList<String> cpfsComputados;
-  private ArrayList<String> pessoasEleitoras;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
 
+  public GerenciamentoVotacao() {
+    this.pessoasCandidatas = new ArrayList<>();
+    this.pessoasEleitoras = new ArrayList<>();
+    this.cpfsComputados = new ArrayList<>();
+  }
+
+  @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata pCandidata : pessoasCandidatas) {
+      if (pCandidata.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
 
+    }
+    PessoaCandidata newCandidata = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(newCandidata);
   }
 
+  @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora pEleitora : pessoasEleitoras) {
+      if (pEleitora.getCpf().equals(cpf)) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
 
+    }
+    PessoaEleitora newEleitora = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(newEleitora);
   }
+
 
   public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
 
   }
+
 
   public void mostrarResultado() {
 
--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -3,7 +3,6 @@ package com.betrybe.sistemadevotacao;
 public class Principal {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
 
   }
 
