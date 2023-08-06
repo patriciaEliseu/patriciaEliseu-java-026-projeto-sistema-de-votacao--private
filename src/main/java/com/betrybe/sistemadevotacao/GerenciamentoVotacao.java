0e6c38937f85ad768675e0afe79db87f4fd55992
Patricia Eliseu <patyeliseu4@gmail.com>
2023-08-05 21:03:16 -0300

    req06 que não passa no teste

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,6 +1,7 @@
 package com.betrybe.sistemadevotacao;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.lang.Math;
 
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
   private ArrayList<PessoaCandidata> pessoasCandidatas;
@@ -14,6 +15,27 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
     this.pessoasCandidatas = new ArrayList<>();
     this.pessoasEleitoras = new ArrayList<>();
     this.cpfsComputados = new ArrayList<>();
+  }
+
+  public ArrayList<PessoaCandidata> getPessoasCandidatas() {
+    return pessoasCandidatas;
+  }
+
+  public void setPessoasCandidatas(ArrayList<PessoaCandidata> pessoasCandidatas) {
+    this.pessoasCandidatas = pessoasCandidatas;
+  }
+
+  public ArrayList<String> getCpfsComputados() {
+    return cpfsComputados;
+  }
+
+
+  public ArrayList<PessoaEleitora> getPessoasEleitoras() {
+    return pessoasEleitoras;
+  }
+
+  public void setPessoasEleitoras(ArrayList<PessoaEleitora> pessoasEleitoras) {
+    this.pessoasEleitoras = pessoasEleitoras;
   }
 
   @Override
@@ -44,11 +66,32 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
 
   public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
-
+    if (cpfsComputados.equals(cpfPessoaEleitora)) {
+      System.out.println("Pessoa eleitora já votou!");
+      return;
+    } else {
+      for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+        if (pessoaCandidata.getNumero() == numeroPessoaCandidata) {
+          pessoaCandidata.receberVoto();
+        }
+      }
+    }
   }
 
 
   public void mostrarResultado() {
-
+    int totalDeVotos = cpfsComputados.size();
+    if (cpfsComputados.isEmpty()) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.");
+    } else {
+      for (PessoaCandidata resultadoCandidato : pessoasCandidatas) {
+        int votosRecebidos = resultadoCandidato.getVotos();
+        float percentualVotos = (votosRecebidos * 100) / totalDeVotos;
+        System.out.println("Nome: " + resultadoCandidato.getNome() + "-"
+            + votosRecebidos + "votos ( " + Math.round(percentualVotos) + "% )");
+      }
+      System.out.println("Total de votos: " + totalDeVotos);
+      return;
+    }
   }
 }
